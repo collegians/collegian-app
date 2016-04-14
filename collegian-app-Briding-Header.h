26892c33aafcb8e8bf644495a07a6adfee50685d
Ahsan Zahid <pie.is.forever@gmail.com>
2016-04-14 11:07:27 -0700

    built

---
+++ collegian-app-Briding-Header.h
@@ -0,0 +1,14 @@
+//
+//  collegian-app-Briding-Header.h
+//  collegian-app
+//
+//  Created by Ahsan Z. Zahid on 4/14/16.
+//  Copyright © 2016 Camille Santos. All rights reserved.
+//
+
+#ifndef collegian_app_Briding_Header_h
+#define collegian_app_Briding_Header_h
+
+#import "MWFeedParser.h"
+
+#endif /* collegian_app_Briding_Header_h */
