43cbefeb9913343ab1db27e9c1573c7609a55f7d
Ahsan Zahid <pie.is.forever@gmail.com>
2016-04-14 11:20:27 -0700

    renamed bridging header

---
+++ collegian-app-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  collegian-app-Bridging-Header.h
+//  collegian-app
+//
+//  Created by Ahsan Z. Zahid on 4/14/16.
+//  Copyright © 2016 Camille Santos. All rights reserved.
+//
+
+#ifndef collegian_app_Bridging_Header_h
+#define collegian_app_Bridging_Header_h
+
+#import "MWFeedParser.h"
+
+#endif /* collegian_app_Bridging_Header_h */
